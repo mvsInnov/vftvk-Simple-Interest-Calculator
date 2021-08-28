b82377f3fe02fc195d9e026a3930570a90d5afaa
Sridhar <mvsinnovations13@gmail.com>
2021-08-28 19:09:24 -0400

    update js

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = document.getElementById("years").value;
+
+    var amount = principal + (principal * years * rate / 100);
+    var year = new Date().getFullYear() + parseInt(years);
+    // interest += principal;
+
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("result").innerHTML = "If you deposit " + principal + ",\<br\> at an interest rate of " +
+        rate + "%.\<br\>You will receive an amount of " + amount + ",\<br\>in the year " + year;
+
 }
-        
+
+// function updateYears() {
+//     var years = document.getElementById("years").value;
+//     var year = new Date().getFullYear() + parseInt(years);
+//     return year;
+// }
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerHTML = rateval + "%";
+}
